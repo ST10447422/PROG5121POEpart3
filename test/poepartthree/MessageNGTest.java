0336d6f5e983ae43391ab4571ac95aec20420b76
ST10447422 <ST10447422@rcconnect.edu.za>
2025-11-20 10:39:30 +0000

    edited message test.

--- test/poepartthree/MessageNGTest.java
+++ test/poepartthree/MessageNGTest.java
@@ -118,8 +118,7 @@ public class MessageNGTest {
         System.out.println("printMessages");
         Message instance = new Message();
         instance.printMessages();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+       
     }
 
     
